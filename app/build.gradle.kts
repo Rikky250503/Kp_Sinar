1aebc4defc8d5d17ab83f7e6c0b8d642cdf8cf5d
michellegra <mchllgrcl@gmail.com>
2024-03-22 17:34:15 +0700

    gimanaa wehhh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,17 +35,17 @@ android {
 }
 
 dependencies {
-
+    
     implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.constraintlayout)
-    implementation(libs.lifecycle.livedata.ktx)
-    implementation(libs.lifecycle.viewmodel.ktx)
-    implementation(libs.navigation.fragment)
-    implementation(libs.navigation.ui)
-    implementation(libs.activity)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+        implementation(libs.material)
+        implementation(libs.constraintlayout)
+        implementation(libs.lifecycle.livedata.ktx)
+        implementation(libs.lifecycle.viewmodel.ktx)
+        implementation(libs.navigation.fragment)
+        implementation(libs.navigation.ui)
+        implementation(libs.activity)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.ext.junit)
+        androidTestImplementation(libs.espresso.core)
 
 }
